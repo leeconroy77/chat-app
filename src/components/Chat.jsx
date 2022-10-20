adbc2a841ebc545e80b25b93ca3e42a6e92599ca
leeconroy <leeconroy1977@yahoo.co.uk>
2022-10-20 19:52:04 +0100

    chat and message components added

---
+++ src/components/Chat.jsx
@@ -0,0 +1,24 @@
+import React, {
+  useState,
+  useEffect, 
+  useRef
+} from 'react'
+import Message from './Message'
+
+const style = {
+  main: `flex flex-col p-[10px] relative`
+}
+
+const Chat = () => {
+
+  const scroll = useRef();
+
+  return (
+    <main className={style.main}>      
+      <Message />
+      <span ref={scroll}></span>
+    </main>
+  )
+}
+
+export default Chat
